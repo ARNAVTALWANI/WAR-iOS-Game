e0b0e7bf87c55f8844ac4b40fdac5be727ae94e0
Arnav T <56046324+ARNAVTALWANI@users.noreply.github.com>
2025-01-21 22:19:51 -0500

    Add files via upload
    Uploaded all assets and Xcode project file

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Arnav Talwani on 2023-12-08.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
